322d5b2c539c74b38d9604d89fcef602ea5b2371
Nikita Mishin <nikita.mishin.91@list.ru>
2023-01-25 00:11:05 +0300

    edit draft

---
+++ script.js
@@ -0,0 +1,86 @@
+// Задание 1
+
+// let password = 'qwerty';
+// let passwordCheck = prompt('Введите пароль');
+// if (passwordCheck === password) {
+//     console.log('Пароль введен верно');
+// } else {
+//     console.log('Пароль введен не верно');
+// };
+
+// Задание 2
+
+// let c = 14;
+// c = 0;
+// if (c > 0 && c < 10) {
+//     console.log('True');
+// } else {
+//     console.log('False');
+// }
+// c = 10;
+// if (c > 0 && c < 10) {
+//     console.log('True');
+// } else {
+//     console.log('False');
+// }
+// c = -3;
+// if (c > 0 && c < 10) {
+//     console.log('True');
+// } else {
+//     console.log('False');
+// }
+// c = 2;
+// if (c > 0 && c < 10) {
+//     console.log('True');
+// } else {
+//     console.log('False');
+// }
+
+// Задание 3
+
+// let d = 46;
+// let e = 64;
+// if (d > 100 || e > 100) {
+//     console.log('True');
+// } else {
+//     console.log('False');
+// }
+
+// Задание 4
+
+// let a = 2;
+// let b = Number('3');
+// console.log(a + b);
+
+// Задание 5
+
+// let monthNumber = 12;
+// let monthNumberAnswer = Number(prompt('Укажите порядковый номер месяца'));
+
+// switch (monthNumber && monthNumberAnswer) {
+//     case 12:
+//     case 1:
+//     case 2:
+//         console.log('Winter');
+//         break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log('Spring');
+//         break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         console.log('Summer');
+//         break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         console.log('Autumn');
+//         break;
+//     default:
+//         console.log('Error');
+//         break;
+// }
+
+// Задание 6
